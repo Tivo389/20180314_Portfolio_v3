65b44fe8ae93a142472e981f81e54e9315f2dd77
Shun Miyaki <shunmiyaki@hotmail.com>
2018-04-10 15:57:20 +0900

    filtering function for experiment apge complete

---
+++ js/experimentFilter.js
@@ -0,0 +1,30 @@
+const categories = document.querySelectorAll('.experiments--category span');
+const experiments = document.querySelectorAll('.experiment--img--container a[data-category]');
+const linkBlocks = document.querySelectorAll('.link--block');
+let experimentHeight;
+
+function filterCategory(e) {
+  let filter = this.dataset.category;
+  experiments.forEach(experiment => {
+    const match = experiment.dataset.category.includes(filter);
+    if(match) {
+      experiment.classList.remove('hidden');
+      experimentHeight = experiment.offsetHeight;
+      console.log(experimentHeight);
+    } else {
+      experiment.classList.add('hidden');
+    }
+  });
+  handleLinkBlock();
+}
+
+function handleLinkBlock() {
+  linkBlocks.forEach(linkBlock => {
+    linkBlock.classList.add('filter--active');
+    linkBlock.style.height = `${experimentHeight}px`;
+  })
+}
+
+categories.forEach(category => {
+  category.addEventListener('click', filterCategory)
+});
